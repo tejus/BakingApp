87db8195b946a4841231e323feaf9f3b7eb2c42b
tejus <tejus.k.v@gmail.com>
2019-04-22 20:33:09 +0530

    Add a basic ListView in MainActivity

--- app/src/main/java/com/tejus/bakingapp/MainActivity.java
+++ app/src/main/java/com/tejus/bakingapp/MainActivity.java
@@ -1,13 +1,26 @@
 package com.tejus.bakingapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.tejus.bakingapp.model.Recipe;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private MainAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Recipe> recipes = Repository.getRecipes(this);
+        mListView = findViewById(R.id.lv_main);
+        mAdapter = new MainAdapter(this, recipes);
+        mListView.setAdapter(mAdapter);
     }
 }
---
+++ app/src/main/java/com/tejus/bakingapp/MainAdapter.java
@@ -0,0 +1,38 @@
+package com.tejus.bakingapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.tejus.bakingapp.model.Recipe;
+
+import java.util.List;
+
+public class MainAdapter extends ArrayAdapter<Recipe> {
+
+    private List<Recipe> mRecipes;
+
+    MainAdapter(Context context, List<Recipe> recipes) {
+        super(context, 0, recipes);
+        mRecipes = recipes;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_2, parent, false);
+        }
+
+        Recipe recipe = mRecipes.get(position);
+        ((TextView) convertView.findViewById(android.R.id.text1)).setText(recipe.getName());
+        ((TextView) convertView.findViewById(android.R.id.text2)).setText(getContext().getString(R.string.main_serves, recipe.getServings()));
+        return convertView;
+    }
+
+    public void setRecipes(List<Recipe> recipes) {
+        mRecipes = recipes;
+    }
+}
