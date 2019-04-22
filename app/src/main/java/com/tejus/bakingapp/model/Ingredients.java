f2b0d47d1627259e8e7fdb282c522041b104847f
tejus <tejus.k.v@gmail.com>
2019-04-22 16:45:03 +0530

    Add model classes for Recipe, Ingredients, and Steps

---
+++ app/src/main/java/com/tejus/bakingapp/model/Ingredients.java
@@ -0,0 +1,38 @@
+package com.tejus.bakingapp.model;
+
+public class Ingredients {
+    private double quantity;
+    private String measure;
+
+    public Ingredients(double quantity, String measure, String ingredient) {
+        this.quantity = quantity;
+        this.measure = measure;
+        this.ingredient = ingredient;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    private String ingredient;
+}
---
+++ app/src/main/java/com/tejus/bakingapp/model/Recipe.java
@@ -0,0 +1,67 @@
+package com.tejus.bakingapp.model;
+
+public class Recipe {
+    private int id;
+    private String name;
+    private Ingredients[] ingredients;
+    private Steps[] steps;
+    private int servings;
+    private String image;
+
+    public Recipe(int id, String name, Ingredients[] ingredients, Steps[] steps, int servings, String image) {
+        this.id = id;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
+        this.servings = servings;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Ingredients[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Ingredients[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Steps[] getSteps() {
+        return steps;
+    }
+
+    public void setSteps(Steps[] steps) {
+        this.steps = steps;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
