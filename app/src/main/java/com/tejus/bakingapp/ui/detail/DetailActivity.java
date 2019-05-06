a2134cc1672a948ef886fbee9b5eefe45ca16f94
tejus <tejus.k.v@gmail.com>
2019-05-06 11:14:01 +0530

    Remove ViewPager for StepsFragment, utilise transactions to replace fragments instead This ensures we get fragment lifecycle callbacks when stepping through the recipe

--- app/src/main/java/com/tejus/bakingapp/ui/detail/DetailActivity.java
+++ app/src/main/java/com/tejus/bakingapp/ui/detail/DetailActivity.java
@@ -3,6 +3,7 @@ package com.tejus.bakingapp.ui.detail;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
@@ -27,11 +28,10 @@ import butterknife.ButterKnife;
 public class DetailActivity extends AppCompatActivity implements StepsOverviewAdapter.OnStepAdapterClickListener {
 
     public static final String EXTRA_POSITION_KEY = "position";
+    public static final String CURRENT_STEP_KEY = "step";
 
     @BindView(R.id.detail_toolbar)
     Toolbar mToolbar;
-    @BindView(R.id.viewpager_steps)
-    ViewPager mViewPager;
     @BindView(R.id.viewpager_overview)
     ViewPager mOverviewViewPager;
     @Nullable
@@ -49,10 +49,11 @@ public class DetailActivity extends AppCompatActivity implements StepsOverviewAd
     @BindView(R.id.tv_overview_steps)
     TextView mTvSteps;
 
+    FragmentManager mFragmentManager;
     private boolean mTwoPane;
     private ActionBar mActionBar;
-    private DetailPagerAdapter mPagerAdapter;
     private Recipe mRecipe;
+    private int mCurrentStep;
     private ViewPagerBottomSheetBehavior mSheetBehavior;
     private int mSheetState;
 
@@ -78,14 +79,14 @@ public class DetailActivity extends AppCompatActivity implements StepsOverviewAd
         if (mActionBar != null) {
             mActionBar.setDisplayHomeAsUpEnabled(true);
             mActionBar.setTitle(mRecipe.getName());
-            mViewPager.addOnPageChangeListener(mStepChangeCallback);
         }
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        mPagerAdapter = new DetailPagerAdapter(fragmentManager, mRecipe);
-        mViewPager.setAdapter(mPagerAdapter);
+        mFragmentManager = getSupportFragmentManager();
+        mCurrentStep = (savedInstanceState != null) ? savedInstanceState.getInt(CURRENT_STEP_KEY) : 0;
+        loadFragment(mCurrentStep);
+
         DetailOverviewPagerAdapter overviewPagerAdapter =
-                new DetailOverviewPagerAdapter(fragmentManager, mRecipe);
+                new DetailOverviewPagerAdapter(mFragmentManager, mRecipe);
         mOverviewViewPager.setAdapter(overviewPagerAdapter);
         mOverviewViewPager.addOnPageChangeListener(mOverviewTabChangeCallback);
 
@@ -100,6 +101,24 @@ public class DetailActivity extends AppCompatActivity implements StepsOverviewAd
         mTvIngredients.post(() -> mTvIngredients.setTranslationY(mTvIngredients.getHeight()));
         mTvSteps.post(() -> mTvSteps.setTranslationY(mTvSteps.getHeight()));
         mSheetBehavior.setBottomSheetCallback(mSheetCallback);
+    }
+
+    private void loadFragment(int position) {
+        mFragmentManager.beginTransaction()
+                .replace(R.id.frame_steps, getStepFragment(position))
+                .commit();
+        updateToolbar();
+    }
+
+    private Fragment getStepFragment(int position) {
+        return StepFragment.newInstance(mRecipe.getSteps().get(position));
+    }
+
+    private void updateToolbar() {
+        if (mCurrentStep == 0)
+            mActionBar.setSubtitle(getString(R.string.detail_intro));
+        else
+            mActionBar.setSubtitle(getString(R.string.detail_step_number, (mCurrentStep)));
     }
 
     private ViewPagerBottomSheetBehavior.BottomSheetCallback mSheetCallback =
@@ -133,23 +152,11 @@ public class DetailActivity extends AppCompatActivity implements StepsOverviewAd
                 }
             };
 
-    private ViewPager.SimpleOnPageChangeListener mStepChangeCallback =
-            new ViewPager.SimpleOnPageChangeListener() {
-                @Override
-                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                    if (position == 0)
-                        mActionBar.setSubtitle(getString(R.string.detail_intro));
-                    else
-                        mActionBar.setSubtitle(getString(R.string.detail_step_number, (position)));
-                }
-            };
-
     public void onBtnClickBack(View v) {
-        if (!mTwoPane
-                && mSheetState != ViewPagerBottomSheetBehavior.STATE_COLLAPSED)
+        if (mCurrentStep == 0 || (!mTwoPane
+                && mSheetState != ViewPagerBottomSheetBehavior.STATE_COLLAPSED))
             return;
-        if (mViewPager.getCurrentItem() > 0)
-            mViewPager.setCurrentItem(mViewPager.getCurrentItem() - 1);
+        loadFragment(--mCurrentStep);
     }
 
     public void onBtnClickOverview(View v) {
@@ -160,11 +167,10 @@ public class DetailActivity extends AppCompatActivity implements StepsOverviewAd
     }
 
     public void onBtnClickForward(View v) {
-        if (!mTwoPane
-                && mSheetState != ViewPagerBottomSheetBehavior.STATE_COLLAPSED)
+        if (mCurrentStep == mRecipe.getSteps().size() - 1 || (!mTwoPane
+                && mSheetState != ViewPagerBottomSheetBehavior.STATE_COLLAPSED))
             return;
-        if (mViewPager.getCurrentItem() < mPagerAdapter.getCount() - 1)
-            mViewPager.setCurrentItem(mViewPager.getCurrentItem() + 1);
+        loadFragment(++mCurrentStep);
     }
 
     public void onBtnClickIngredients(View v) {
@@ -183,7 +189,8 @@ public class DetailActivity extends AppCompatActivity implements StepsOverviewAd
 
     @Override
     public void onStepClick(int position) {
-        mViewPager.setCurrentItem(position);
+        mCurrentStep = position;
+        loadFragment(mCurrentStep);
         if (!mTwoPane)
             onBackPressed();
     }
@@ -234,5 +241,11 @@ public class DetailActivity extends AppCompatActivity implements StepsOverviewAd
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(CURRENT_STEP_KEY, mCurrentStep);
     }
 }
