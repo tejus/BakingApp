e6fce635cb0d972f5c516f5c8028dbd32f4180d5
tejus <tejus.k.v@gmail.com>
2019-05-11 21:38:40 +0530

    Test BottomSheetBehavior and Ingredient/Step ViewPagers on <600dp width layouts

---
+++ app/src/androidTest/java/com/tejus/bakingapp/DetailActivityPhoneBasicTest.java
@@ -0,0 +1,67 @@
+package com.tejus.bakingapp;
+
+import android.content.Intent;
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.tejus.bakingapp.model.Recipe;
+import com.tejus.bakingapp.ui.detail.DetailActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class DetailActivityPhoneBasicTest {
+
+    @Rule
+    public ActivityTestRule<DetailActivity> mActivityTestRule =
+            new ActivityTestRule<>(DetailActivity.class, false, false);
+
+    /**
+     * Tests the BottomSheetBehavior along with Ingredient and Step ViewPager
+     * on layouts with width less than 600dp
+     */
+    @Test
+    public void clickOverviewStep_ShowsStepRecyclerView() {
+        Recipe recipe = Recipe.getTestRecipe();
+        Intent testLaunchIntent = new Intent();
+        testLaunchIntent.putExtra(DetailActivity.EXTRA_RECIPE_KEY, recipe);
+        mActivityTestRule.launchActivity(testLaunchIntent);
+        onView(withId(R.id.tv_overview)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.rv_ingredients)).check(matches(isDisplayed()));
+        onView(withId(R.id.tv_overview_steps)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.rv_steps)).check(matches(isDisplayed()));
+        onView(withId(R.id.tv_overview_ingredients)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.rv_ingredients)).check(matches(isDisplayed()));
+        Espresso.pressBack();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.tv_step_heading)).check(matches(isDisplayed()));
+    }
+}
